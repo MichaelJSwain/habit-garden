50d5d78d8d67620407e9600ef54abed9cc2a3f05
MichaelSwain <michaeljswain@gmail.com>
2025-04-21 09:13:14 +0200

    store days in current month in state

--- src/components/CheckinHeatmap/CheckinHeatmap.jsx
+++ src/components/CheckinHeatmap/CheckinHeatmap.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import './CheckinHeatmap.css';
 
 export const CheckinHeatmap = ({history}) => {
+    const [daysInMonth, setDaysInMonth] = useState(Array.from(Array(30).keys()));
+
     return (
     <div className="check-in-heatmap">
-        
-        {Array.from(Array(30).keys()).map((item, idx) => {
+        {daysInMonth.map((day, idx) => {
             return <div key={idx} className={(history.some(item => item === `2025-04-${idx + 1}`) )? "heatmap-table-cell active" : "heatmap-table-cell"}></div>
         })}
     </div>
